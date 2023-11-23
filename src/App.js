61f56ef69ab040a46e147b8ee56d854eda45cf0c
el-moudni-hicham <hichamelmoudni7@example.com>
2023-11-23 14:05:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [currentRoute, setCurrentRoute] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <Link onClick= {()=> setCurrentRoute("Home")} 
+                  className= {currentRoute == 'Home' ? 'nav-item nav-link active' : 'nav-item nav-link'} 
+                  to={"/"}>
+            Home</Link>
+
+            <Link onClick= {()=> setCurrentRoute("Products")} 
+                  className={currentRoute == 'Products' ? 'nav-item nav-link active' : 'nav-item nav-link'} 
+                  to={"/Products"}>
+            Products</Link>
+          </div> 
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/new" element={<NewProduct />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
